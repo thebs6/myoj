4d1e38ed7dae897592b5bea8de330dad04ac580b
thebs <thebs6@163.com>
2023-02-01 10:05:15 +0800

    add: InetAddress

---
+++ src/InetAddress.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstdint>
+#include <netinet/in.h>
+#include <iostream>
+
+
+class InetAddress
+{
+public:
+    InetAddress() = delete;
+    explicit InetAddress(uint16_t port = 0, std::string ip = "127.0.0.1");
+    explicit InetAddress(const sockaddr_in& addr) : addr_(addr) {}
+    std::string toIp() const;
+    uint16_t toPort() const;
+    std::string toIpPort() const;
+
+    inline const sockaddr_in* getSockAddr() const { return &addr_; };
+    inline void setSockAddr(const sockaddr_in& addr) { addr_ = addr; };
+private:
+    sockaddr_in addr_;
+};
--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,13 @@
 #include <iostream>
 #include "Timestamp.h"
+#include "InetAddress.h"
 
 int main(int argc, const char** argv) {
     std::cout << "fuck off!" << std::endl;
     std::cout << Timestamp::now().toString() << std::endl;
+    InetAddress addr(8080);
+    std::cout << addr.toIpPort() << std::endl;
+    std::cout << addr.toIp() << std::endl;
+    std::cout << addr.toPort() << std::endl;
     return 0;
 }
