424b5797169e3ce4fac46e020aa5573f37904dfb
thebs <thebs6@163.com>
2023-02-05 16:38:28 +0800

    fix: 命名规范

--- src/Callbacks.h
+++ src/Callbacks.h
@@ -10,6 +10,6 @@ class Timestamp;
 using TcpConnectionPtr = std::shared_ptr<TcpConnection>;
 using ConnectionCallback = std::function<void(const TcpConnectionPtr&)>;
 using CloseCallback = std::function<void(const TcpConnectionPtr&)>;
-using writeCompleteCallback = std::function<void(const TcpConnectionPtr &)>;
+using WriteCompleteCallback = std::function<void(const TcpConnectionPtr &)>;
 using highWaterMarkCallbakc = std::function<void(const TcpConnectionPtr &, size_t)>;
 using MessageCallback = std::function<void(const TcpConnectionPtr &, Buffer*, Timestamp)>;
