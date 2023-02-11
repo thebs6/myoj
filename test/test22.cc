c7b36f0dd635168ac6441d888a843ec8ea0553f1
thebs <thebs6@163.com>
2023-02-11 14:47:51 +0800

    move main.cc to test/

--- test/test22.cc
+++ test/test22.cc
@@ -9,16 +9,6 @@
 #include <string>
 #include "TcpConnection.h"
 #include "Logger.h"
-// int main(int argc, const char** argv) {
-//     std::cout << "fuck off!" << std::endl;
-//     std::cout << Timestamp::now().toString() << std::endl;
-//     InetAddress addr(8080);
-//     std::cout << addr.toIpPort() << std::endl;
-//     std::cout << addr.toIp() << std::endl;
-//     std::cout << addr.toPort() << std::endl;
-//     std::weak_ptr<void> ptr;
-//     return 0;
-// }
 
 class EchoServer
 {
